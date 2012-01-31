c8b47b8dd607f99906e171a226eb2b4a232504b9
Jeff Chapman II <jac@JAChapmanII.net>
2012-01-31 17:20:38 -0500

    Add simple connect/print/disconnect, update Makefile

--- Makefile
+++ Makefile
@@ -5,10 +5,10 @@ BDIR=.
 BINS=$(BDIR)/quincy $(BDIR)/cm
 MAPS=map/vmap map/simap map/ismap
 QOBJS=$(ODIR)/vmap.o $(ODIR)/simap.o $(ODIR)/ismap.o $(ODIR)/util.o
-MOBJS=
+MOBJS=$(ODIR)/ircsock.o $(ODIR)/cbuffer.o
 
 LDFLAGS=-pthread
-CFLAGS=-std=c99 -pedantic -Wall -Wextra
+CFLAGS=-std=c99 -D_POSIX_C_SOURCE=200112L -D_BSD_SOURCE -pedantic -Wall -Wextra
 
 ifndef RELEASE
 CFLAGS+=-g
@@ -39,5 +39,5 @@ map/%: map/%.def map/%.dec map/%.def
 	$(BDIR)/mstruct.sh $< map $(SDIR)
 
 clean:
-	rm -f $(ODIR)/*.o $(BDIR)/$(BIN)
+	rm -f $(ODIR)/*.o $(BINS)
 
--- conman/cm.c
+++ conman/cm.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+#include "ircsock.h"
+
 int main(int argc, char **argv) {
+	printf("Creating isock...\n");
+	IRCSock *isock = ircsock_create("irc.freenode.net", 6667, "quincy");
+	if(isock == NULL) {
+		fprintf(stderr, "main: could not create isock\n");
+		return 1;
+	}
+	printf("Connecting isock...\n");
+	if(ircsock_connect(isock) != 0) {
+		fprintf(stderr, "main: isock could not connect\n");
+		return 2;
+	}
+	printf("Joining isock...\n");
+	if(ircsock_join(isock, "#zebra")) {
+		fprintf(stderr, "main: isock could not join\n");
+		return 3;
+	}
+	printf("Sending pmsg...\n");
+	ircsock_pmsg(isock, "#zebra", "Hello, there!");
+	for(int i = 0; i < 3; ++i)
+		usleep(1000*1000);
+
+	printf("Quiting...\n");
+	ircsock_quit(isock);
+	usleep(1000*1000);
+	ircsock_free(isock);
 	return 0;
 }
 
