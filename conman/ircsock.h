fe8cc439c8a3be13cf378cd31b1e642ee2726892
Jeff Chapman II <jac@JAChapmanII.net>
2012-02-01 16:27:36 -0500

    Switch IRCSock to be non-blocking, update struct and comments

--- conman/ircsock.h
+++ conman/ircsock.h
@@ -4,7 +4,8 @@
 #include <sys/types.h>
 #include "cbuffer.h"
 
-#define IRCSOCK_MERROR -2
+#define IRCSOCK_MERROR   -2
+#define IRCSOCK_BUF_SIZE 4096
 
 /* IRCSock is a structure used to maintain a bidirectional connection to an
  * IRC server. It provides facilities to resolve the domain name, connect the
@@ -18,23 +19,23 @@ typedef struct {
 	char *host;
 	int port;
 	int domain;
+	int socket;
 
 	char *nick;
 
 	char *chan;
 
-	int socket;
-
-	/* This is a circular buffer where read data is stored, line by line. */
-	CBuffer *cbuf;
+	char buf[IRCSOCK_BUF_SIZE];
+	char wbuf[IRCSOCK_BUF_SIZE];
 } IRCSock;
 
-/* Create an IRCSock, copies all data into an IRCSock *.
+/* Create an IRCSock, copies all data into an IRCSock
  *
  * Returns pointer to new IRCSock, or
- * 	NULL on failure (does not leak)
+ * 	NULL on failure
  */
 IRCSock *ircsock_create(char *host, int port, char *nick);
+
 /* Frees the memory associated with an IRCSock.
  *
  * Note: Does not disconnect properly from connected socket
@@ -48,22 +49,25 @@ void ircsock_free(IRCSock *ircsock);
  */
 int ircsock_connect(IRCSock *ircsock);
 
-/* Does a blocking read on the socket. Puts read data into ->cbuf
+/* Does a nonblocking read on the socket.
  *
- * Returns the amount read, or
- * 	-1 on underlying read error (sets errno)
- * 	IRCSOCK_MERROR on malloc error during string copying (prints to stderr)
+ * Returns a null-terminated string the user is required to free, or
+ * 	NULL if there was no data to be returned
  */
-ssize_t ircsock_read(IRCSock *ircsock);
+char *ircsock_read(IRCSock *ircsock);
+
 /* Write data to socket, ending with required \r\n
  *
  * Returns amount written (expect 2 + strlen(str)), or
+ * 	0 on delayed write
  * 	-1 on write error (sets errno)
  */
 ssize_t ircsock_send(IRCSock *ircsock, char *str);
+
 /* Formats a PRIVMSG command using target and message, then sends it
  *
- * Returns amount sent (expect strlen("PRIVMSG " ~ target ~ " " ~ msg) + 2), or
+ * Returns amount sent (expect strlen("PRIVMSG " ~ target ~ " :" ~ msg) + 2), or
+ * 	0 on delayed write
  * 	-1 on write error (sets errno)
  * 	IRCSOCK_MERROR on malloc error
  */
@@ -76,9 +80,11 @@ ssize_t ircsock_pmsg(IRCSock *ircsock, char *target, char *msg);
  * 	other value -- on corresponding IRC response code
  */
 int ircsock_join(IRCSock *ircsock, char *chan);
+
 /* Sends QUIT command through socket
  *
  * Returns 6 on success (strlen("QUIT") + 2), or
+ * 	0 on delayed write
  * 	-1 on failure (sets errno)
  */
 int ircsock_quit(IRCSock *ircsock);
