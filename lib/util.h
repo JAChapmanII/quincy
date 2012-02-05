5f4546b175842e6b9785fd44b59339d2915d9bb1
Jeff Chapman II <jac@JAChapmanII.net>
2012-02-05 16:30:06 -0500

    Comment the functions available in util

--- lib/util.h
+++ lib/util.h
@@ -4,16 +4,35 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// Write a size_t to a file in a endian independant way
 size_t util_writeSizeT(FILE *outFile, size_t toWrite);
+// Read a size_t from a file in a endian independant way
 size_t util_readSizeT(FILE *inFile);
 
+// Close both ends of the passed pipe
 int util_closePipe(int *fds);
+// Check to see if a file exists (that is, if we can open it for reading)
 int util_exists(const char *fileName);
+
+/* Create a pipe to a subprocess
+ * 	binary: the name of the binary to invoke
+ * 	argv: this array is passed to the subprocess as its main's argv
+ * 	fds: an int[2] to store the read/write ends of the pipe
+ */
 int util_subprocessPipe(const char *binary, char **argv, int *fds);
 
+// Set a file descriptor to non blocking mode
 int util_setNonBlocking(int fd);
-int util_blockreadReady(int fd);
 
+/* Extract a string from the specified buf
+ * 	buf: the string buffer
+ * 	bufSize: the size of the buffer
+ * 	split: the string to split the buf on
+ *
+ * If split is found in buf, then the part leading up to split is returned as
+ * a string (owned by caller). The rest of the buf (minus the split string) is
+ * then moved back, padding the end of buf with null bytes.
+ */
 char *util_fetch(char *buf, size_t bufSize, char *split);
 
 #endif // UTIL_H
