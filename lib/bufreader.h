49d19dd9d816cf30aa5c35677c23433e99065e6e
Jeff Chapman II <jac@JAChapmanII.net>
2012-02-07 08:36:50 -0500

    Add some poor comments describing BufReader's purpose

--- lib/bufreader.h
+++ lib/bufreader.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 
+// BufReader provides buffered read support from a file descriptor.
 typedef struct {
 	int fd;
 	char *split;
@@ -12,9 +13,16 @@ typedef struct {
 	int eof;
 } BufReader;
 
+// Construct a BufReader with a given signal string and buffer size
 BufReader *bufreader_create(int fd, char *split, size_t bufSize);
+// Free memory associated with a BufReader and close the backing fd
 void bufreader_free(BufReader *br);
 
+/* Perform a nonblocking read on the underlying fd.
+ *
+ * If the split string was found in the buffer (or one was read), this returns
+ * a valid stirng owned by the caller.
+ */
 char *bufreader_read(BufReader *br);
 
 #endif // BUFREADER_H
